a683e06bb0ae40311afd8d70fb0aba867ca2e008
Mikko Ravimo <mikko.ravimo@gmail.com>
2014-04-18 10:16:33 +0300

    NEW: Trackmodel to abstract the trackpiece handling

---
+++ java/src/main/java/hwo/kurjatturskat/model/TrackModel.java
@@ -0,0 +1,28 @@
+package hwo.kurjatturskat.model;
+
+import hwo.kurjatturskat.core.message.gameinit.TrackPieces;
+import hwo.kurjatturskat.util.LoopingList;
+
+import java.util.Arrays;
+
+public class TrackModel {
+
+    private LoopingList<TrackPieces> pieces;
+
+    public TrackModel(TrackPieces pieces[]) {
+        this.pieces = new LoopingList<>(Arrays.asList(pieces));
+    }
+
+    public TrackPieces getCurrent() {
+        return pieces.getCurrent();
+    }
+
+    public TrackPieces getNext() {
+        return pieces.getNext();
+    }
+
+    public void setCurrentPiece(int yourPieceIndex) {
+        pieces.setCurrent(yourPieceIndex);
+    }
+
+}
