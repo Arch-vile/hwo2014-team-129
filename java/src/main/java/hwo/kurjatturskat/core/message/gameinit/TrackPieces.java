c474caefe7f51517d9cafe473b9dc23ccda6531c
Mikko Ravimo <mikko.ravimo@gmail.com>
2014-04-18 10:17:46 +0300

    Helper to tell if it is a curve piece

--- java/src/main/java/hwo/kurjatturskat/core/message/gameinit/TrackPieces.java
+++ java/src/main/java/hwo/kurjatturskat/core/message/gameinit/TrackPieces.java
@@ -1,13 +1,17 @@
 package hwo.kurjatturskat.core.message.gameinit;
 
 public class TrackPieces {
-    public final double length;
-    public final double radius;
-    public final double angle;
+    public final Double length;
+    public final Double radius;
+    public final Double angle;
 
-    public TrackPieces(double length, double radius, double angle) {
+    public TrackPieces(Double length, Double radius, Double angle) {
         this.length = length;
         this.radius = radius;
         this.angle = angle;
+    }
+
+    public boolean isCurve() {
+        return this.angle != null;
     }
 }
