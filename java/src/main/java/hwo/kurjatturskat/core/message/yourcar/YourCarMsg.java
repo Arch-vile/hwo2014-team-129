d49021047b330273c8e6ee25795591499d8680c7
Mikko Ravimo <mikko.ravimo@gmail.com>
2014-04-18 07:59:03 +0300

    Replace YourCarData with CarIdentifier

--- java/src/main/java/hwo/kurjatturskat/core/message/yourcar/YourCarMsg.java
+++ java/src/main/java/hwo/kurjatturskat/core/message/yourcar/YourCarMsg.java
@@ -1,11 +1,12 @@
 package hwo.kurjatturskat.core.message.yourcar;
 
+import hwo.kurjatturskat.core.message.CarIdentifier;
 import hwo.kurjatturskat.core.message.Message;
 import hwo.kurjatturskat.core.message.MessageType;
 
-public class YourCarMsg extends Message<YourCarData> {
+public class YourCarMsg extends Message<CarIdentifier> {
 
-    public YourCarMsg(YourCarData data) {
+    public YourCarMsg(CarIdentifier data) {
         super(MessageType.yourCar, data);
     }
 }
