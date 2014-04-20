4a214d4328de514574535cc43a1ce09699b96279
Mikko Ravimo <mikko.ravimo@gmail.com>
2014-04-20 09:16:05 +0300

    Add missing switch data field

--- java/src/main/java/hwo/kurjatturskat/core/message/gameinit/TrackPieces.java
+++ java/src/main/java/hwo/kurjatturskat/core/message/gameinit/TrackPieces.java
@@ -1,14 +1,21 @@
 package hwo.kurjatturskat.core.message.gameinit;
 
+import com.google.gson.annotations.SerializedName;
+
 public class TrackPieces {
     public final Double length;
     public final Double radius;
     public final Double angle;
 
-    public TrackPieces(Double length, Double radius, Double angle) {
+    @SerializedName("switch")
+    public final boolean isSwitch;
+
+    public TrackPieces(Double length, Double radius, Double angle,
+            boolean isSwitch) {
         this.length = length;
         this.radius = radius;
         this.angle = angle;
+        this.isSwitch = isSwitch;
     }
 
     public boolean isCurve() {
