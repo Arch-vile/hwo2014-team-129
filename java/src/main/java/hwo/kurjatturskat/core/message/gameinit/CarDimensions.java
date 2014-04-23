64a33a241501afdc683da72adae9c3dee3f3632e
Markus Heikkilä <markus.heikkila@gmail.com>
2014-04-23 17:44:05 +0300

    Fix car dimensions.

--- java/src/main/java/hwo/kurjatturskat/core/message/gameinit/CarDimensions.java
+++ java/src/main/java/hwo/kurjatturskat/core/message/gameinit/CarDimensions.java
@@ -3,11 +3,11 @@ package hwo.kurjatturskat.core.message.gameinit;
 import hwo.kurjatturskat.core.message.CarIdentifier;
 
 public class CarDimensions {
-    public CarIdentifier car;
+    public CarIdentifier id;
     public Dimensions dimensions;
 
     public CarDimensions(CarIdentifier car, Dimensions dimensions) {
-        this.car = car;
+        this.id = id;
         this.dimensions = dimensions;
     }
 }
